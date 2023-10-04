f4662332e010b85c61985e3ec00cf180b2346162
Aporrhia <147005378+Aporrhia@users.noreply.github.com>
2023-10-05 01:55:58 +0300

    Delete Student_List directory

--- Student_List/web2/transferData.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (isset($_POST['transfer-form'])) {
-    // Include the code for transferring data from DB2 to DB1
-    include('updatePerson.php');
-    include('updateThesis.php');
-}
-
-?>
